3f9ddffe769f3274b75697d3ae4d2e7852227629
Marianne Äikäs <marianne.aikas@gmail.com>
2021-09-18 15:41:16 +0300

    Added hello.py

---
+++ Lab05/Hello.py
@@ -0,0 +1,11 @@
+#import a datetime library to access current date and time
+import datetime
+
+#Print my own name
+print("Marianne Äikäs")
+
+#print the current
+print(datetime.datetime.now())
+
+#Print "Hello World!"
+print("Hello World!")
