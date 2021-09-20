e7d716b9a4e2ff3eec99da837f0400c7dd747727
Marianne Äikäs <AB6258@Labranet.jamk.fi>
2021-09-20 15:38:08 +0300

    added mjono and age demos

---
+++ Testit/age.py
@@ -0,0 +1,11 @@
+#Tee ohjelma, joka laskee henkilön iän syntymävuoden perusteella
+
+#ika = input("Anna syntymävuotesi: ")
+#vuosi = int(2021)
+#tulos = int(vuosi) - int(ika)
+#print(tulos)
+
+year = int(input("Anna syntymävuotesi: "))
+thisyear = 2021
+age = (thisyear - year)
+print(f"Olet {age} vuotta vanha.")
---
+++ Testit/demo_säde.py
@@ -0,0 +1,4 @@
+sade = int(input("Anna säde: "))
+pinta_ala = int(3.14) * sade^2
+
+print("Pinta-ala: ")
