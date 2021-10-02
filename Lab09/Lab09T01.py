57e2e726148157baaf49dc6524157499aabbc534
Marianne Äikäs <marianne.aikas@gmail.com>
2021-10-02 15:36:23 +0300

    Added testversion and real 09T01

--- Lab09/Lab09T01.py
+++ Lab09/Lab09T01.py
@@ -1,11 +1,6 @@
-a = int(input("Insert number (rainfall): "))
-b = int(input("Insert number (rainfall): "))
-c = int(input("Insert number (rainfall): "))
-d = int(input("Insert number (rainfall): "))
-e = int(input("Insert number (rainfall): "))
-f = int(input("Insert number (rainfall): "))
-g = int(input("Insert number (rainfall): "))
+numList = []
 
-number_list = (a, b, c, d, e, f, g)
+for i in range(7):
+    numList.append(int(input("Insert number (rainfall): ")))
 
-print(f"Rainfall sum is {sum(number_list)}")
+print(f"Rainfall sum is {sum(numList)}")
