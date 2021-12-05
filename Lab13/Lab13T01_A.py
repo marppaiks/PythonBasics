60ca34063ba0449a2e6cf1813d3f01ceef9b0b48
AB6258 <ab6258@student.jamk.fi>
2021-12-05 16:31:06 +0200

    Lisätty toimeksianto kommenttina

--- Lab13/Lab13T01_A.py
+++ Lab13/Lab13T01_A.py
@@ -1,3 +1,7 @@
+#Toteuta ohjelma, joka kysyy käyttäjältä autojen rekisterinumeroita (siis esim 'ABC-123' jne) ja tallentaa ne listaan.
+#Käyttäjä voi syöttää niin monta rekisterinumeroa kuin haluaa, syöttäminen lopetetaan tyhjällä syötteellä.
+#Näytä syötetyt rekisterinumero aakkosjärjestyksessä.
+
 carlist = []
 
 while True:
