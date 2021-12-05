eaad3abed01d97a2d207e94132a7edec5482f962
AB6258 <ab6258@student.jamk.fi>
2021-12-05 16:21:22 +0200

    Lisätty toimeksianto kommenttina

--- Lab09/Lab09T05.py
+++ Lab09/Lab09T05.py
@@ -1,3 +1,6 @@
+#Tee funktio lotto(), joka arpoo lottorivin seitsemän (7) numeroa väliltä 1-40 ja palauttaa sen merkkijonona, luvut eroteltuna pilkuilla.
+#Siis esimerkiksi näin:   1,3,5,10,20,33,39
+
 def lotto():
     import random
     return random.randrange(1, 41)
