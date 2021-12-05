90b3d218a83b4e4721b0b69a2c7fb46437c44bd8
AB6258 <ab6258@student.jamk.fi>
2021-12-05 16:30:01 +0200

    Lisätty toimeksianto kommenttina

--- Lab12/Lab12T03.py
+++ Lab12/Lab12T03.py
@@ -1,3 +1,13 @@
+#Autotallissasi on kolme autoa. Tee luokka Car. Tee luokalla on kolme ominaisuutta brand, model ja price.
+#Luo Car-luokasta vähintään kolme erilaista auto-oliota. Aseta autojen ominaisuudet seuraavasti:
+
+# brand="Skoda" model="Octavia" price=3000
+# brand="Audi" model="A4" price=4000
+# brand="Volvo" model="V70" price=5000
+
+#Tulosta kaikkien autotallin kolmen auton ominaisuudet konsolille.
+#Laske myös autojen yhteishinta, ja näytä se konsolilla
+
 class Car:
     def __init__(self, brand = "", model = "", price = 0): #Luodaan muodostaja
         self.brand = brand
