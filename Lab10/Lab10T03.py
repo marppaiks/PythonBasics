f837fba551f397875aa4d41bd78074d6b3d3ea4f
AB6258 <ab6258@student.jamk.fi>
2021-12-05 16:22:59 +0200

    Lisätty toimeksianto kommenttina

--- Lab10/Lab10T03.py
+++ Lab10/Lab10T03.py
@@ -1,3 +1,5 @@
+#Tee funktio average, jolle viedään 3 parametria. Average funktio palauttaa annettujen parametrien keskiarvon yhden desimaalin tarkkuudella.
+
 def average(num1, num2, num3):
     keskiarvo = (num1 + num2 + num3) / 3
     format_keskiarvo = "{:.1f}".format(keskiarvo)
