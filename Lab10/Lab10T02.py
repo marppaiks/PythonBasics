9a7b5f82f829a0d6158aec7c390dc370a6a73e7a
AB6258 <ab6258@student.jamk.fi>
2021-12-05 16:22:31 +0200

    Lisätty toimeksianto kommenttina

--- Lab10/Lab10T02.py
+++ Lab10/Lab10T02.py
@@ -1,3 +1,5 @@
+#Tee funktio subtract, jolle viedään 2 parametria. Funktion tulee palauttaa annettujen parametrien erotuksen.
+
 def subtract(number1, number2):
     erotus = number1 - number2
     return erotus
