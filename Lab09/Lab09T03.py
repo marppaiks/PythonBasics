275c96e6aa29f3bd2b8aac941ab677b06c138fc7
AB6258 <ab6258@student.jamk.fi>
2021-12-05 16:19:15 +0200

    Lisätty toimeksianto kommenttina

--- Lab09/Lab09T03.py
+++ Lab09/Lab09T03.py
@@ -1,3 +1,6 @@
+#Tee ohjelma, joka kysyy ensin käyttäjältä jonkin luvun väliltä 1-10.
+#Tämän jälkeen näytä luvut yhdestä annettuun lukuun ja luvun neliön.
+
 numList = []
 luku = int(input("Anna numero väliltä 1-10: "))
 
