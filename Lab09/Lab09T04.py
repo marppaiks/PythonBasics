22e57a2e0e8f0e5fdd6a5ab67992b500dbee9cff
AB6258 <ab6258@student.jamk.fi>
2021-12-05 16:19:59 +0200

    Lisätty toimeksianto kommenttina

--- Lab09/Lab09T04.py
+++ Lab09/Lab09T04.py
@@ -1,3 +1,7 @@
+#Tee ohjelma, joka kysyy käyttäjältä käyttäjän etu ja sukunimen.
+#Tulosta käyttäjän etunimen ensimmäistä kirjainta niin monta kertaa kun etunimessä on kirjaimia.
+#Tämän jälkeen tulosta käyttäjän sukunimi käänteisessä järjestyksessä.
+
 etunimi = input("Anna etunimi: ")
 sukunimi = input("Anna sukunimi: ")
 
