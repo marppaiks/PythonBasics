96c79524597eaf782bb7d49c579560e6305e8885
AB6258 <ab6258@student.jamk.fi>
2021-12-05 16:31:36 +0200

    Lisätty toimeksianto kommenttina

--- Lab13/Lab13T02_A.py
+++ Lab13/Lab13T02_A.py
@@ -1,3 +1,7 @@
+#Tee ohjelma joka kysyy käyttäjältä kurssien arvosanoja (arvosana on kokonaisuluku 0,1,2,3,4 tai 5) ja tallentaa ne listaan.
+#Käyttäjä voi syöttää niin monta kurssiarvosanaa kuin haluaa, syöttäminen lopetetaan tyhjällä syötteellä.
+#Näytä lopuksi montako arvosanaa käyttäjä antoi ja arvosanojen keskiarvo.
+
 gradeslist = []
     
 while True:
