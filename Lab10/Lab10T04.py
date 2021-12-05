2bcedd664dab56a1c75604d0e90c2bbf936c0972
AB6258 <ab6258@student.jamk.fi>
2021-12-05 16:24:06 +0200

    Lisätty toimeksianto kommenttina

--- Lab10/Lab10T04.py
+++ Lab10/Lab10T04.py
@@ -1,3 +1,13 @@
+#Tee funktio calc_consumption. Sinne viedään parametreina:
+
+# auton kulutus litraa/100km
+# polttoaineen hinta euroa per litra
+# kuljettu matka kilometreinä.
+# calc_consumption-funktio palauttaa tietoina kuinka monta litraa polttoainetta on kulunut matkalla sekä polttoaineeseen kuluneen rahan määrän.
+#Kysy käyttäjältä:
+#kulutus, polttoaineen hinta ja kuljettu matka.
+#Sen jälkeen kutsu calc_consumption-funktiota ohjelmasta. Tarkista että funktio laskee kulutuksen ja polttoaineen hinnan oikein.
+
 def calc_consumption(kulutus, hinta, matka):
     kokonaiskulutus = (kulutus / 100) * matka
     kokonaishinta = kokonaiskulutus * hinta
