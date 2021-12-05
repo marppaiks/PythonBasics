4aece388520c05774cdecfec75ccc855b1bd4046
AB6258 <ab6258@student.jamk.fi>
2021-12-05 16:06:05 +0200

    Lisätty toimeksianto kommenttina

--- Lab07/lab07t01.py
+++ Lab07/lab07t01.py
@@ -1,2 +1,4 @@
+#Tee ohjelma, joka tulostaa konsoliin tekstin "Here starts basics of programming with Python". Lisää toinen rivi, jossa on etunimesi ja sukunimesi välilyönnillä erotettuna, siis esim "Jakke Jäyhä".
+
 print("Here starts basics of programming with python")
 print("Marianne Äikäs")
