fdeb1abebc2d8df5888676135eb4bd1ad90c7736
AB6258 <ab6258@student.jamk.fi>
2021-12-05 16:33:03 +0200

    Lisätty toimeksianto kommenttina

--- Lab14/Lab14T01.py
+++ Lab14/Lab14T01.py
@@ -1,3 +1,8 @@
+#Tee ohjelma, jossa yrität muuttaa listan sellaista arvoa, jota ei ole olemassa.
+#Alusta siis lista, jossa on neljä elementtiä ja sen jälkeen yritä muuttaa viidettä elementtiä.
+#Tarkista millaisen poikkeuksen saat.
+#Korjaa ohjelma niin ettei se kaadu.
+
 lista = [1,2,3,4]
 
 try:
