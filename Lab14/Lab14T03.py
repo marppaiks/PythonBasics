cc159c4938a01428d058cc7e4a0f73e0bbb26ee3
AB6258 <ab6258@student.jamk.fi>
2021-12-05 16:34:50 +0200

    Lisätty toimeksianto kommenttina

--- Lab14/Lab14T03.py
+++ Lab14/Lab14T03.py
@@ -1,3 +1,10 @@
+#Toteuta funktio isthiszero(num). Funktiolla välitetään yksi parametri. Funktio palauttaa true jos parametrin arvo on nolla.
+#Funktio palauttaa false, jos parametri on luku mutta ei nolla. Funktio nostaa TypeError-poikkeuksen, jos parametri ei ole luku.
+
+#Kokeile kutsua  ohjelmasta funktioita eri arvoilla. Toteuta kutsuvalla ohjelmalle try - except. Mitä havaitset?
+#Vastaukset tehtävän alkuun kommentteina.
+
+
 #Havaitsin, että tehtävänanto on epäselvä.
 #En osannut tulkita, haluttiinko tässä että numeroiksi käy vain intit ja floatit, vai haluttiinko myös str-tyyppiset numerot.
 #Ohjelma tulkitsee nyt str-tyyppiset TypeErrorina, vaikka niissä ei olisi kirjaimia.
