fe39ca7ba70fc0dc98285d14d5dc3be0a6413032
AB6258 <ab6258@student.jamk.fi>
2021-12-05 16:32:16 +0200

    Lisätty toimeksianto kommenttina

--- Lab13/Lab13T03.py
+++ Lab13/Lab13T03.py
@@ -1,3 +1,9 @@
+#Toteuta ohjelma, johon voi tallentaa kymmenen eri auton tiedot.
+#Kustakin autosta tiedetään rekisterinumero (esim ABC-123) ja autonmerkki (esim Skoda).
+
+#Keksi itse erilaisia rekisterinumeroita ja automerkkejä. Tallenna tiedot valitsemaasi kokoelmaan.
+#Tulosta sen jälkeen autojen tiedot ensin aakkosjärjestyksssä automerkin mukaan, ja sen jälkeen aakkosjärjestyksessä rekisterinumeron mukaan.
+
 cars = []
 
 while True: # Autojen lisääminen listaan käyttäjän syöttämillä tiedoilla
