d3ec30bb56b9603a475bb7b391e627c3d34a7069
AB6258 <ab6258@student.jamk.fi>
2021-12-05 16:17:31 +0200

    Lisätty toimeksianto kommenttina

--- Lab09/Lab09T01.py
+++ Lab09/Lab09T01.py
@@ -1,3 +1,7 @@
+#Tee ohjelma, joka kysyy käyttäjältä viikon kunkin päivän sademäärän.
+#Sademäärä annetaan kokonaislukuna, jollei kyseisenä päivänä ole satanut käyttäjä antaa luvuksi 0.
+#Laske ja näytä viikon kokonaissademäärä.
+
 numList = []
 
 for i in range(7):
